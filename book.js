dc3ac9b4b7a1cbcabbb42c67bebe239f9a8296c8
陳 佳冬 <jiadong.chen@g.softbank.co.jp>
2019-10-19 01:19:09 +0900

    [add] package update

--- book.js
+++ book.js
@@ -1,24 +1,29 @@
 module.exports = {
-      plugins: [
-            "uml", // PlantUml
-            "mindmap",
-            "mathjax", // 数式
-            "anchor-navigation-ex-toc", // 章節番号自動付与
-            "-sharing", // SNSマーク消し
-            "hide-published-with", // Publishリンク消し
-            "wide-page", // ページのWideを可変にする
-      ],
-      pluginsConfig: {
-            "anchor-navigation-ex-toc": {
-                  associatedWithSummary: true,
-                  multipleH1: false,
-                  showLevel: true
-            },
-            "mathjax": {
-                  forceSVG: true
-            },
-            "theme-default": {
-                  showLevel: true
-            }
-      }
+  plugins: [
+    "uml", // PlantUml
+    "mindmap",
+    "mathjax", // 数式
+    "anchor-navigation-ex-toc", // 章節番号自動付与
+    "-sharing", // SNSマーク消し
+    "hide-published-with", // Publishリンク消し
+    "wide-page", // ページのWideを可変にする
+    "expand-active-chapter"
+  ],
+  pluginsConfig: {
+    uml: {
+      charset: "utf-8",
+      format: "svg"
+    },
+    "anchor-navigation-ex-toc": {
+      associatedWithSummary: true,
+      multipleH1: false,
+      showLevel: true
+    },
+    mathjax: {
+      forceSVG: true
+    },
+    "theme-default": {
+      showLevel: true
+    }
+  }
 };
